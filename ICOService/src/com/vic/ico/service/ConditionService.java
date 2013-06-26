e321f79537169f57e0d9bbfe47fc85f0275fb9d9
VIC <yoonsica@vip.qq.com>
2013-06-26 23:11:45 +0800

    提交了ICOService工程

---
+++ ICOService/src/com/vic/ico/service/ConditionService.java
@@ -0,0 +1,7 @@
+package com.vic.ico.service;
+
+import java.util.List;
+
+public interface ConditionService {
+	public List<String> getConditionsHtml(String pageId);
+}
---
+++ ICOService/src/com/vic/ico/types/ConditionType.java
@@ -0,0 +1,6 @@
+package com.vic.ico.types;
+
+public enum ConditionType {
+	select,
+	checkbox;
+}
